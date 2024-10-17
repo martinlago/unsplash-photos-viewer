8d8dca66623df0d4a81228912ad0d89aa452934e
Martin Lago <martin.lago@houlak.paigo.com.uy>
2024-10-16 22:18:49 -0300

    Add Xcode project

---
+++ UnsplashPhotosViewer/UnsplashPhotosViewer/UnsplashPhotosViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashPhotosViewerApp.swift
+//  UnsplashPhotosViewer
+//
+//  Created by Martin Lago on 16/10/24.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashPhotosViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
